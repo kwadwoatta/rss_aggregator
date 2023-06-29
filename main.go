6e24089e69bce0e1d13ab292a8710215f74cd269
Prince Ofori <princeofori065@gmail.com>
2023-06-29 08:21:33 +0000

    feat: add user handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
@@ -9,47 +10,67 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	database "github.com/kwadwoatta/rss_aggregator/internal/database"
+	_ "github.com/lib/pq"
 )
 
+type apiConfig struct {
+	DB *database.Queries
+}
+
 func main() {
-	err := godotenv.Load()
-	if err != nil {
+	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
-	} else {
 
-		portString := os.Getenv("PORT")
+	}
 
-		if portString == "" {
-			log.Fatal("PORT is not found in the environment")
-		}
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
 
-		router := chi.NewRouter()
-		router.Use(middleware.Logger)
-		router.Use(cors.Handler(cors.Options{
-			AllowedOrigins:   []string{"https://*", "http://*"},
-			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-			ExposedHeaders:   []string{"Link"},
-			AllowCredentials: false,
-			MaxAge:           300,
-		}))
+	}
 
-		v1Router := chi.NewRouter()
-		v1Router.Get("/healthz", handlerReadiness)
-		v1Router.Get("/err", handlerErr)
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
 
-		router.Mount("/v1", v1Router)
+	connection, err := sql.Open("postgres", dbURL)
 
-		server := &http.Server{
-			Handler: router,
-			Addr:    ":" + portString,
-		}
+	if err != nil {
+		log.Fatal("Can't connect to database")
+	}
 
-		err := server.ListenAndServe()
+	apiCfg := apiConfig{
+		DB: database.New(connection),
+	}
 
-		if err != nil {
-			log.Fatal(err)
-		}
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.handlerGetUser)
+
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
 	}
 
 }
